17dd5dbb1481078dfabd8b9d429fa4bd48d42976
nzf210 <syahrilsymd@gmail.com>
2023-02-20 15:46:46 +0800

    chg contract

--- contracts/WchToken.sol
+++ contracts/WchToken.sol
@@ -1,35 +1,49 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract WanCash is Initializable, ERC20Upgradeable, UUPSUpgradeable {
-    address public owner;
-    uint256 public maxSupply;
-    modifier onlyOwner() {
-        require(owner == _msgSender(), "Not authorized");
+contract WanCash is  ERC20Capped, ERC20Burnable {
+    address payable public owner; 
+    uint256 public reward;
+
+    constructor(uint256 _initialSupply, uint256 _reward) 
+    ERC20("WanCash","WCH") 
+    ERC20Capped(_initialSupply * (10 ** decimals()))
+    {
+        owner = payable(msg.sender);
+        _mint(owner , _initialSupply * (10 ** decimals()));
+        reward = _reward * (10 ** decimals()); 
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner,"Only tHe Owner call this function");
         _;
     }
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor(uint256 _maxSupply) initializer {
-        maxSupply = _maxSupply;
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
     }
 
-    function initialize(address initialOwner) initializer public {
-        __ERC20_init("Wancash", "WCH");
-        __UUPSUpgradeable_init();
-
-        owner = initialOwner;
+    function setReward(uint256 _reward) public onlyOwner {
+        reward = _reward * (10 ** decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        require(maxSupply <= (amount + address(this).balance));
-        _mint(to, amount);
+    function _mintReward() internal {
+        _mint(block.coinbase, reward);
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    function _beforeTokenTransfer(address _from, address _to, uint256 _value)internal virtual override {
+        if(_from != address(0) && _to != block.coinbase && block.coinbase != address(0)){
+            _mintReward();
+        }
+        super._beforeTokenTransfer(_from, _to, _value);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,45 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "wandev",
+  networks: {
+    wandev: {
+      url: `wss://apitest.wanchain.org:8443/ws/v3/${process.env.WAN_API_KEY}`,
+      chainId: 999,
+      accounts: [process.env.WANDEV_KEY]
+    },
+    wancash: {
+      url: `wss://api.wanchain.org:8443/ws/v3/${process.env.WAN_API_KEY}`,
+      accounts: [process.env.WANDEV_KEY]
+    },
+    wandev2: {
+      url: `https://gwan-ssl.wandevs.org:46891`,
+      chainId: 999,
+      gasPrice: 20000000000,
+      accounts: [process.env.WANDEV_KEY]
+    },
+    wancash2: {
+      url: `https://gwan-ssl.wandevs.org:56891`,
+      chainId: 888,
+      gasPrice: 20000000000,
+      accounts: [process.env.WANDEV_KEY]
+    },
+    bscdev: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      // accounts: { mnemonic: mnemonic }
+      accounts: [process.env.WANDEV_KEY]
+    },
+    bscmain: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      // accounts: { mnemonic: mnemonic }
+      accounts: [process.env.WANDEV_KEY]
+    }
+  }
 };
+
